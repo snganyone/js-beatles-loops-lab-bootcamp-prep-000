b2b85f09f30505c355bffbfdb6a656dffc711d69
Shishiri Nganyone <snganyone1@gmail.com>
2020-04-13 03:44:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // add solution here
+
+function johnLennonFacts(array){
+  let index = 0
+  while(index < array.length){
+    console.log(array + "!!!")
+  }
+}
+
+/*function iLoveTheBeatles(num){
+  let arr = []
+  do{
+
+  }while()
+}*/
